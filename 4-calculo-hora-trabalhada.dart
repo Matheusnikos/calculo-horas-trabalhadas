95eba6f15d9f793e6868a0c654a790db137bd6b3
Matheus Nikos <matheusnikos.izaias@gmail.com>
2023-10-30 20:20:35 -0300

    Input adicionado

--- 4-calculo-hora-trabalhada.dart
+++ 4-calculo-hora-trabalhada.dart
@@ -1,8 +1,37 @@
+import 'dart:io';
+
 void main() {
-  num salarioTotal = 1212;
-  num horasTrabalhadas = 220;
+  print(
+      '== Bem-vindo! Nesse programa você irá descobrir qual o valor das suas horas trabalhadas! ==');
+  print("\x1B[2J\x1B[0;0H");
 
-  num valorHoraTrabalhada = salarioTotal / horasTrabalhadas;
+  num valorSalario = pegarValorSalario();
+  print("\x1B[2J\x1B[0;0H");
 
-  print(valorHoraTrabalhada);
+  num valorHoras = pegarValorHoras();
+  print("\x1B[2J\x1B[0;0H");
+
+  num valorHoraTrabalhada = valorSalario / valorHoras;
+
+  print('O valor da sua hora trabalhada é de $valorHoraTrabalhada reais');
+}
+
+pegarValorSalario() {
+  print('Insira o valor total do seu salário');
+  String? valorSalarioString = stdin.readLineSync();
+  if (valorSalarioString == null) {
+    return 0.0;
+  } else {
+    return num.parse(valorSalarioString);
+  }
+}
+
+pegarValorHoras() {
+  print('Insira o valor total de horas trabalhadas');
+  String? valorHorasString = stdin.readLineSync();
+  if (valorHorasString == null) {
+    return 0.0;
+  } else {
+    return num.parse(valorHorasString);
+  }
 }
